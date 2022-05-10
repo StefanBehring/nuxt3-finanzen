a87a266a6a4b991104225f690dbdb54e3497e269
StefanBehring <chadhz@gmx.de>
2022-05-10 17:09:31 +0200

    Add component InputText

---
+++ components/inputs/InputText.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="input-area">
+    <label :for="id">{{ label }}</label>
+    <input
+      type="text"
+      :name="id"
+      :placeholder="placeholder"
+      :value="value"
+      @input="handleInput"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  id: {
+    type: String,
+    required: true,
+  },
+  label: {
+    type: String,
+    required: false,
+  },
+  placeholder: {
+    type: String,
+    required: false,
+  },
+  content: {
+    type: String,
+    required: false,
+    default: "",
+  },
+});
+
+const emit = defineEmits(["update:content"]);
+
+const value = ref(props.content);
+
+const handleInput = (event: Event) => {
+  const target = event.target as HTMLInputElement;
+  value.value = target.value;
+  emit("update:content", target.value);
+};
+</script>
+
+<style lang="css"></style>
