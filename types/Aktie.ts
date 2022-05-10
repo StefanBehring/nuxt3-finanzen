a2efb6c85832315c3bd8c6caa3ac57aca225687f
StefanBehring <chadhz@gmx.de>
2022-05-10 17:09:43 +0200

    Add types

---
+++ types/Aktie.ts
@@ -0,0 +1,10 @@
+type Aktie = {
+  id: any
+  unternehmen: string
+  wkn: string
+  isin: string
+  symbol: string
+  link: string
+}
+
+export default Aktie
---
+++ types/InputTextFeld.ts
@@ -0,0 +1,8 @@
+type InputTextFeld = {
+  id: string
+  label: string
+  placeholder: string
+  value: string
+}
+
+export default InputTextFeld
